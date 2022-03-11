0bd3d8440c4811d45b40dc05704e6d5c680b849d
Пользователь <example@gmail.com>
2022-03-11 22:28:03 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const namberofFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const perconalMovieDB = {
+    count: namberofFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На скуолько отцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На скуолько отцените его?', '');
+
+
+perconalMovieDB.movies[a] = b;
+perconalMovieDB.movies[c] = d;
+
+console.log(perconalMovieDB);
