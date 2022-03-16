74178cdf066dc091e2a6e848767e2959a4da2151
Пользователь <example@gmail.com>
2022-03-16 18:46:37 +0600

    first commit

--- script.js
+++ script.js
@@ -8,14 +8,27 @@ const perconalMovieDB = {
     privat: false
 };
 
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На скуолько отцените его?', '');
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На скуолько отцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На скуолько отцените его?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        perconalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
-
-perconalMovieDB.movies[a] = b;
-perconalMovieDB.movies[a] = d;
+if (perconalMovieDB.count < 10) {
+    console.log("Прсмотрено доволно мало фильмов");
+} else if (perconalMovieDB.count >= 10 && perconalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (perconalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Призошла обшибка");
+}
 
 console.log(perconalMovieDB);
