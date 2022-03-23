f63521870ff8befde21a86d825344e1d1e7de5a0
Пользователь <example@gmail.com>
2022-03-23 18:56:41 +0600

    second

--- script.js
+++ script.js
@@ -1,34 +1,9 @@
-const namberofFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+"use strict";
 
-const perconalMovieDB = {
-    count: namberofFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+const str = "teSt";
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На скуолько отцените его?', '');
+// console.log(str[2] = 'd')
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        perconalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
-}
-
-if (perconalMovieDB.count < 10) {
-    console.log("Прсмотрено доволно мало фильмов");
-} else if (perconalMovieDB.count >= 10 && perconalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-} else if (perconalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-} else {
-    console.log("Призошла обшибка");
-}
-
-console.log(perconalMovieDB);
+//console.log(str.toLocaleUpperCase());
+//console.log(str.loLowerCase());
+console.log(str);
